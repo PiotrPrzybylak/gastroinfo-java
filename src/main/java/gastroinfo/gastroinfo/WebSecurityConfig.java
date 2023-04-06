ab82ac1695dc53e89539c9a7d4d376bfb1f0eb2b
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-04-06 23:52:11 +0200

    Make /api require login

--- src/main/java/gastroinfo/gastroinfo/WebSecurityConfig.java
+++ src/main/java/gastroinfo/gastroinfo/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
-                                .requestMatchers("/admin/*")
+                                .requestMatchers("/api/**")
                                 .authenticated()
                                 .anyRequest().permitAll()
                 )
