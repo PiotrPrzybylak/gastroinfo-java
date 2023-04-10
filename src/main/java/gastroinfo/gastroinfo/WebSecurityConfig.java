8cd05855131caa64ba6715e02ebc3db269c57cc4
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-04-10 13:46:43 +0200

    Require login for admin page

--- src/main/java/gastroinfo/gastroinfo/WebSecurityConfig.java
+++ src/main/java/gastroinfo/gastroinfo/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
-                                .requestMatchers("/api/**")
+                                .requestMatchers("/api/**", "/yummy-town-admin/**")
                                 .authenticated()
                                 .anyRequest().permitAll()
                 )
